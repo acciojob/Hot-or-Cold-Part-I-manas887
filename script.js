fe8ba9c74952bc1b8d54ff626a235cb41a7163b7
manas887 <116465782+manas887@users.noreply.github.com>
2023-02-14 19:32:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 //your code here
+var num = document.getElementById("num");
+var btn = document.getElementById("btn");
+var inputnum = document.getElementById("guess").value;
+var result = document.getElementById("respond");
+
+function generatenumber(){
+	  var gnum = (Math.floor(Math.random() * 40) - 20 );
+		num.innerHTML = gnum;
+	var diff = inputnum - gnum; 
+	if(diff <= 5 && diff >= 0){
+		result.innerHTML = "Hot";
+	}else{
+		result.innerHTML = "Cold";
+	}
+}
+
+btn.addEventListener('click', generatenumber);
 
