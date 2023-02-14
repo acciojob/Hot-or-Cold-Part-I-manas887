16de59ab877d1060e04c46e132e6fc29ba2ed7f0
manas887 <116465782+manas887@users.noreply.github.com>
2023-02-14 19:37:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ var result = document.getElementById("respond");
 function generatenumber(){
 	  var gnum = (Math.floor(Math.random() * 40) - 20 );
 		num.innerHTML = gnum;
-	var diff = inputnum - gnum; 
-	if(diff <= 5 && diff >= 0){
+	var diff = Number(inputnum) - Number(gnum); 
+	if( Math.abs(diff) <= 5 ){
 		result.innerHTML = "Hot";
 	}else{
 		result.innerHTML = "Cold";
